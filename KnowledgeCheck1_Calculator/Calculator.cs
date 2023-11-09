3af3c207487992eec46328e76e08c8783cfb4423
Tony Garrett <tony.garrett@gmail.com>
2023-11-08 20:44:51 -0500

    initial commit

---
+++ KnowledgeCheck1_Calculator/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace KnowledgeCheck1_Calculator
+{
+    public class Calculator
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        public int Subtract(int first, int second)
+        {
+            return first - second;
+        }
+
+        public int Multiply(int first, int second)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Divide(double first, double second)
+        {
+            return first / second;
+        }
+    }
+}
